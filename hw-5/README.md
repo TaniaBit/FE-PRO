d72ceec9c993430409daa4983b93f4d123cd6916
TaniaBit <bitkot@ukr.net>
2025-05-26 13:15:20 +0300

    hw-5 User interaction

---
+++ hw-5/README.md
@@ -0,0 +1,2 @@
+Create a simple page where when you apply for a request, ask for a name and confirm that you want privacy.
+In response to the notification, output an alert() to do nothing.
---
+++ hw-5/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// declaration variable nameUser 
+
+let nameUser = prompt("What's your name?");
+
+// confirmation of greeting output + nameUser
+
+if (confirm("Do you want to see the greeting?"))
+    {
+alert("Hello, " + nameUser + "! How are you?");
+}
+
+
